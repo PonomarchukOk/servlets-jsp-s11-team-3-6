3fb289fde3c7feab0167f23c02cdbf717e66b51f
RMuskovets <rmuskovets@gmail.com>
2021-01-04 11:17:35 +0200

    Finish #1 and #2
    2 new servlets: CreateTaskServlet and HomeServlet. They handle creating new tasks and serving the home page.
    Also I added a toolbar.jsp in pages/ to not edit every single page when we get the task list.

--- src/main/java/com/softserve/itacademy/controller/TestServlet.java
+++ src/main/java/com/softserve/itacademy/controller/TestServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(value = "/")
+@WebServlet(name = "TestServlet", value = "/test")
 public class TestServlet extends HttpServlet {
 
     private TaskRepository taskRepository;
