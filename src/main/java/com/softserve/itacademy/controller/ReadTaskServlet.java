999a813d78fdb6377960ce019452b5769ae4b66f
RMuskovets <rmuskovets@gmail.com>
2021-01-05 17:32:37 +0200

    Finish #5
    Adds a ReadTaskServlet and JSP page for it: read-task.jsp.

---
+++ src/main/java/com/softserve/itacademy/controller/ReadTaskServlet.java
@@ -0,0 +1,33 @@
+package com.softserve.itacademy.controller;
+
+import com.softserve.itacademy.model.Task;
+import com.softserve.itacademy.repository.TaskRepository;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "ReadTaskServlet", value = "/read-task")
+public class ReadTaskServlet extends HttpServlet {
+    private TaskRepository taskRepository;
+
+    @Override
+    public void init() {
+        taskRepository = TaskRepository.getTaskRepository();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String idStr = request.getParameter("id");
+        if (idStr != null) {
+            Task task = taskRepository.read(Integer.parseInt(idStr));
+            request.setAttribute("task", task);
+            request.getRequestDispatcher("/WEB-INF/pages/read-task.jsp").forward(request, response);
+        } else {
+            response.sendError(404, "Not Found");
+        }
+    }
+}
